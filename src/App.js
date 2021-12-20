132965090efc4fb04a09989ac378395aa26e8b21
yusuf wijaya <yusufwijaya3@gmail.com>
2021-12-20 20:50:39 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavbarComponent from './components/NavbarComponents';
+import swal from 'sweetalert';
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponent />
+      <h2>Halo</h2>
+    </div>
+  );
+}
+
+export default App;
